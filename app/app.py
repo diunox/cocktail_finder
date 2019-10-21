bb97441af552e0c1024014fdce8d147f57e70de8
Austin Black <austinblack1@gmail.com>
2019-10-21 00:50:02 +0000

    update requirements and syntax

--- app/app.py
+++ app/app.py
@@ -3,6 +3,8 @@ from flask import render_template
 from flask import request
 import requests, json
 import config
+import mysql.connector
+
 app = Flask(__name__)
 
 
@@ -29,6 +31,14 @@ def results(ingredient1=None):
     ingredient1 = request.form['ingredient1']
     ingredient2 = request.form['ingredient2']
     ingredient3 = request.form['ingredient3']
+
+    cnx = mysql.connector.connect(host='db-ablack-demo-do-user-6644004-0.db.ondigitalocean.com', port=25060, user='doadmin', db='defaultdb', passwd='hym61r4n0jypa93w', ssl_ca='ca-certificate.crt')
+    cursor = cnx.cursor()
+    cursor.execute("insert into ingredients (ingredient, ing_count) values ('%s', 1) on duplicate key update ing_count = ing_count + 1") % ingredient1
+    cursor.execute("insert into ingredients (ingredient, ing_count) values ('%s', 1) on duplicate key update ing_count = ing_count + 1") % ingredient2
+    cursor.execute("insert into ingredients (ingredient, ing_count) values ('%s', 1) on duplicate key update ing_count = ing_count + 1") % ingredient3
+    cursor.commit()
+
     ing1_drinks = construct_json(ingredient1)
     ing2_drinks = construct_json(ingredient2)
     ing3_drinks = construct_json(ingredient3)
